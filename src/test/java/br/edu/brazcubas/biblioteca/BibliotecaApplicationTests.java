3e44bd1d787ca89b2705fb4180e23db0dae8c0ab
rafael <rafaelcorrea2154r@gmail.com>
2024-04-28 17:34:37 -0300

    initial commit

---
+++ src/test/java/br/edu/brazcubas/biblioteca/BibliotecaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.brazcubas.biblioteca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
