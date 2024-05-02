0471b13b229d889df6b7fce4255272c02b64cd0c
rafael <rafaelcorrea2154r@gmail.com>
2024-05-01 22:47:21 -0300

    update: add new objects to main

--- src/main/java/br/edu/brazcubas/biblioteca/BibliotecaApplication.java
+++ src/main/java/br/edu/brazcubas/biblioteca/BibliotecaApplication.java
@@ -1,13 +1,29 @@
 package br.edu.brazcubas.biblioteca;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import br.edu.brazcubas.biblioteca.controller.LivroController;
+import br.edu.brazcubas.biblioteca.model.dao.LivroDAO;
+import br.edu.brazcubas.biblioteca.model.database.DBConfig;
+import br.edu.brazcubas.biblioteca.model.entity.Livro;
+import br.edu.brazcubas.biblioteca.view.LivroView;
 
 @SpringBootApplication
 public class BibliotecaApplication {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws SQLException {
 		SpringApplication.run(BibliotecaApplication.class, args);
-	}
+		DBConfig.createTables(DBConfig.getConnection());
 
+		LivroController livroCon = new LivroController(new LivroDAO());
+		LivroView livroView = new LivroView();
+		Scanner scan = new Scanner(System.in);
+
+		SistemaBiblioteca sys = new SistemaBiblioteca(livroCon, livroView, scan);
+		sys.inicializarSistema();
+	}
 }
